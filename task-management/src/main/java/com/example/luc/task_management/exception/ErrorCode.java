e1d28d578c6bac6a2525d1f5ea74f341c36a69ec
Phạm Tiến Lực <52300042@student.tdtu.edu.vn>
2026-05-31 03:19:03 +0700

    Update ErrorCode.java

--- task-management/src/main/java/com/example/luc/task_management/exception/ErrorCode.java
+++ task-management/src/main/java/com/example/luc/task_management/exception/ErrorCode.java
@@ -13,11 +13,24 @@ public enum ErrorCode {
     TOKEN_EXPIRED       (HttpStatus.UNAUTHORIZED,  "Token has expired"),
     UNAUTHORIZED        (HttpStatus.UNAUTHORIZED,  "You are not login in yet"),
 
+    // User
+    WRONG_PASSWORD      (HttpStatus.BAD_REQUEST, "The current password is incorrect."),
+    PASSWORD_NOT_MATCH  (HttpStatus.BAD_REQUEST, "Password confirmation does not match."),
+    SAME_PASSWORD       (HttpStatus.BAD_REQUEST, "The new password must not be the same as the old password"),
+
     // Resource
     USER_NOT_FOUND      (HttpStatus.NOT_FOUND,     "User does not exists"),
     BOARD_NOT_FOUND     (HttpStatus.NOT_FOUND,     "Board does not exists"),
     COLUMN_NOT_FOUND    (HttpStatus.NOT_FOUND,     "Column does not exists"),
     TASK_NOT_FOUND      (HttpStatus.NOT_FOUND,     "Task does not exists"),
+
+    BOARD_ARCHIVED(HttpStatus.BAD_REQUEST, "The board have been saved, the operation cannot be performed"),
+
+
+    // Label
+    LABEL_NOT_FOUND     (HttpStatus.NOT_FOUND,   "Label does not exists"),
+    LABEL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,  "The label name already exists on the board"),
+    LABEL_IN_USE        (HttpStatus.BAD_REQUEST,  "Label are being used by several tasks"),
 
     // Permission
     FORBIDDEN           (HttpStatus.FORBIDDEN,     "You aren't authorize to do this"),
